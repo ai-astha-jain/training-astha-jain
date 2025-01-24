2ecaa18d0a45da625fb8d29556c7ea9ac69688cf
ai-astha-jain <astha.jain@icreativetechnolabs.com>
2025-01-24 15:42:27 +0530

    [UPDATED]

---
+++ decorators_modify.py
@@ -0,0 +1,62 @@
+import time 
+from datetime import datetime
+def decorator(input_num):
+
+    global start_time_code
+    start_time_code = datetime.now()
+    #print("The time when code is started : ", start_time_code)
+    
+    while True:
+        print("\n To calculate :\n")
+        print(" 1. ADD\n 2. SUBTRACT\n 3. MULTIPLY\n 4. DIVIDE \n 0. EXIT")
+        choice = input("Enter the operation choice you want to perform(1/2/3/4/0): ")
+        if choice.isdigit():
+            choice = int(choice)
+        else:
+            print("Invalid choice. Please enter the valid choice")
+            continue
+        if choice == 1:
+            num1 = input_num()
+            num2 = input_num()
+            print("The addition is: ", f"{num1} + {num2} = {num1 + num2}")
+        elif choice == 2:
+            num1 = input_num()
+            num2 = input_num()
+            print("The subtraction is: ", f"{num1} - {num2} = {num1 - num2}")
+        elif choice == 3:
+            num1 = input_num()
+            num2 = input_num()
+            print("The multiply is: ", f"{num1} * {num2} = {num1 * num2}")
+        elif choice == 4:
+            num1 = input_num()
+            num2 = input_num()
+            try:
+                print("The division is: ", f"{num1} / {num2} = {num1 / num2}")
+            except ZeroDivisionError:
+                print("Invalid input.Please enetr valid input.")
+        elif choice == 0:
+            break
+        elif choice > 4:
+            print("Please Enter the choice between 1 to 4")
+        else:
+            raise TypeError("Only integers are allowed")
+   
+       
+@decorator	   
+def input_num(): 
+    while True:
+        try:
+            num1 = int(input("Enter the number: "))
+            if isinstance(num1, (int,float)):
+                return num1
+            else:
+                print("Please enter the valid integers.")
+                continue
+        except (NameError, ValueError):
+            print("Invalid input. please enter the integers")
+            continue
+
+global end_time_code
+end_time_code = datetime.now()
+#print("The time when code has ended: ", end_time_code)
+print("total time: " ,(end_time_code - start_time_code), "seconds")
