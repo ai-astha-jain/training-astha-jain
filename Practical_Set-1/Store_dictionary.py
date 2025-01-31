5717c4b6903dd7e09e290214d105292de5f6ebdc
ai-astha-jain <astha.jain@icreativetechnolabs.com>
2025-01-31 16:28:25 +0530

    [UPDATED]

--- Practical_Set-1/Store_dictionary.py
+++ Practical_Set-1/Store_dictionary.py
@@ -9,37 +9,36 @@ class Store:
                 num_shelves = int(input("Enter the number of shelves: "))
             except ValueError:
                 print("Please enter the valid integer")
-                return num_shelves            
+                return True         
             # loop to enter shelf name and store data into dictionary
             for i in range(num_shelves):
-                
                 shelf_name = input("Enter the shelf name: ")
-                if shelf_name != shelf_name.isdigit():
+                if not shelf_name.isdigit():
                     self.store_dictionary[shelf_name] = {}
                 else:
-                    print("Enter the valid shelf name.")
-                    return shelf_name            
+                    print("Please enter the valid input")
+                    return True            
                 # Enter the number of products in the specific shelf
                 try:
                     num_products = int(input(f"Enter the number of products you want in {shelf_name}: "))
                 except ValueError:
                     print("Please enter the valid integer")
-                    return num_products
+                    return True
                 #Number of products in the choosen shelf
                 for i in range(num_products):
                     product_name = input(f"Enter the product name in {shelf_name}: ")
-                    if product_name != product_name.isdigit():
+                    if not product_name.isdigit():
                         self.store_dictionary[shelf_name][product_name] = {}
                     else:
                         print("Enter the valid product name.")
-                        return product_name      
+                        return True      
                 
                     # months for which data is available
                     try:
-                        months = input(f"Enter the months for {product_name} in {shelf_name} (comma separated): ").split(',')
+                        months = list(map(str,input("Enter the months for {product_name} in {shelf_name} (comma separated): ").split(',')))
                     except ValueError:
                         print("Please enter the months separated by comma.")
-                        return months
+                        return True
                         
                     for month in months:
                         month = month.strip()
@@ -47,13 +46,13 @@ class Store:
                             costs = list(map(int, input(f"Enter the cost price for {product_name} in {month} (space separated): ").split()))
                         except ValueError:
                             print("Please enter the cost price with the space and enter a valid integer.")
-                            return costs
+                            return True
                             
                         try:
                             sale_percentage = int(input(f"Enter the sale percentage for {product_name} in {month}: "))
                         except ValueError:
                             print("Please enter the sale percentage in integer.")
-                            return sale_percentage
+                            return True
                             
                         self.store_dictionary[shelf_name][product_name][month] ={
                             "cost_price": costs,
@@ -95,7 +94,7 @@ class Store:
     def set_category_product(self, shelf_name, product_name):
         if shelf_name in self.store_dictionary:
             if product_name in self.store_dictionary[shelf_name]:
-                category = input("Enter the category for {product_name} in shelf {shelf_name}: ")
+                category = input(f"Enter the category for {product_name} in shelf {shelf_name}: ")
                 
                 self.store_dictionary[shelf_name][product_name]["category"] = category
                 
@@ -108,17 +107,30 @@ class Store:
     #method to create a new shelf        
     def create_new_shelf(self):
         shelf_name = input("Enter the shelf name:")
+        if not shelf_name.isdigit():
+            self.store_dictionary[shelf_name] = {}
+        else:
+            print("Please enter the valid input")
+            return True  
         if shelf_name not in self.store_dictionary:
             print(f"Shelf {shelf_name} not found. Create new shelf.")
             self.store_dictionary[shelf_name] = {}
             
             # Enter the number of products in the specific shelf
-            num_products = int(input(f"Enter the number of products you want in{shelf_name}: "))
+            try:
+                num_products = int(input(f"Enter the number of products you want in {shelf_name}: "))
+            except ValueError:
+                print("Please enter the valid integer")
+                return True
             
             #Number of products in the choosen shelf
             for i in range(num_products):
                 product_name = input(f"Enter the product name in {shelf_name}: ")
-                self.store_dictionary[shelf_name][product_name] = {}       
+                if not product_name.isdigit():
+                    self.store_dictionary[shelf_name][product_name] = {}
+                else:
+                    print("Enter the valid product name.")
+                    return True 
                 
                 # months for which data is available
                 months = input(f"Enter the months for {product_name} in {shelf_name} (comma separated): ").split(',')
@@ -126,8 +138,11 @@ class Store:
                 for month in months:
                     month = month.strip()
                     costs = list(map(int, input(f"Enter the cost price for {product_name} in {month} (space separated): ").split()))
-                    sale_percentage = int(input(f"Enter the sale percentage for {product_name} in {month}: "))
-                        
+                    try:
+                        sale_percentage = int(input(f"Enter the sale percentage for {product_name} in {month}: "))
+                    except ValueError:
+                        print("Please enter the sale percentage in integer.")
+                        return True    
                     self.store_dictionary[shelf_name][product_name][month] ={
                         "cost_price": costs,
                         "sale_price": [cp + (cp*sale_percentage/100) for cp in costs]
@@ -225,8 +240,10 @@ class Store:
                 new_sale_percentage = float(input("Enter the new sale percentage to update for all products: "))
                 self.update_product_sale_price_for_all(new_sale_percentage)
             elif choice == 4:
-                new_sale_percentage_shelf = float(input("Enter the new sale percentage to update for all products: "))
-                self.update_product_sale_price_for_all(new_sale_percentage_shelf)
+            	shelf_name = input("Enter the shelf name: ")
+            	product_name = input("Enter the product name: ")
+            	new_sale_percentage_shelf = float(input("Enter the new sale percentage to update for all products: "))
+            	self.update_product_sale_price_for_all(new_sale_percentage_shelf)
             elif choice == 5:
                 shelf_name = input("Enter the shelf name: ")
                 product_name = input("Enter the product name: ")
